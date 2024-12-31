f56f1365d5bbadff0515e4d2a381c062069b2560
ItzRae <rachellin443@gmail.com>
2024-12-30 20:59:50 -0500

    test: add assets

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,7 @@ const config = {
         }),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/raes portfolio' : '',
+			assets: 'https://github.com/ItzRae/raes-portfolio'
         },
         appDir: 'internal',
     }
